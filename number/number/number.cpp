4f62b2e1a2da4b9d34089d3968d8731faa51609f
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-08-20 13:34:54 +0300

    Add files via upload

---
+++ number/number/number.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+ struct Person {
+	std::string name = "";
+	unsigned int age;
+
+} ;
+
+
+
+
+int main()
+{
+
+	// Локализация
+
+	setlocale(LC_ALL, "Ru");
+
+	int a = 5.1; // класическая инициализация
+
+	int b{ 1 }; // Инициализация списком
+
+	std::cout << a << std::endl;
+
+	// Инициализация списком
+
+	std::cout << b << std::endl;
+
+	// Обьявляем структуру через ввод cin;
+
+	Person p1;
+
+	std::cout << "Введите ваше имя: ";
+	std::cin >> p1.name;
+	std::cout << "Введите ваш возраст: ";
+	std::cin >> p1.age;
+
+	std::cout << "Привет,  " << p1.name << "тебе " << p1.age << " лет!" << std::endl;
+
+
+
+
+}
+
